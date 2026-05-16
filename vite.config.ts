25cd37e9939a3084b3f1b47b89198b9052a90f8b
Leonardo Afonso <162568940+HLeonardoAfonso@users.noreply.github.com>
2026-05-16 16:10:39 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './',
+  base: '/PhaserCorp/',
   server: {
     port: 3000,
   },
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]',
+      }
+    }
   },
 });
